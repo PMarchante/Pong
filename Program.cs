1fd9f35ed57b944ca1f2e0a48665b2f4951bba40
PMarchante <35638610+PMarchante@users.noreply.github.com>
2019-07-23 11:59:32 -0400

    started the game area

--- Program.cs
+++ Program.cs
@@ -4,9 +4,62 @@ namespace SpaceGame
 {
     class Program
     {
+
+        public char[][] gameSpace()
+        {
+            char[][] space = new char[30][];
+
+            for (int x = 0; x < space.Length; x++)
+            {
+
+
+                for (int y = 0; y < space.Length; y++)
+                {
+                    //if (x == 30)
+                    space[x][y] = '-';
+                }
+                Console.WriteLine(space);
+            }
+
+            return space;
+        }
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+            Program test = new Program();
+            for (int x = 0; x == 100; x++)
+            {
+                Console.WriteLine(test.gameSpace());
+            }
+            */
+
+            string[,] space = new string[20, 30];
+
+            for (int x = 0; x < 20; x++)
+            {
+
+                Console.Write("\n");
+                for (int y = 0; y < 30; y++)
+                {
+                    if (y >= 29)
+                    {
+                        space[x, y] = "                                                            |";
+                    }
+
+                    if (x == 0 || x == 19)
+                    {
+                        space[x, y] = "__";
+                    }
+
+                    Console.Write(space[x, y]);
+                }
+
+            }
+
+
         }
     }
 }
